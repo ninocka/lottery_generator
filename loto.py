1d24bc3109e2e1677c70010eaf23ba2013f377c1
ninocka <ninahajtnik@gmail.com>
2017-04-21 22:19:11 +0200

    first

---
+++ loto.py
@@ -0,0 +1,21 @@
+import random
+
+def lottery():
+    a = int(raw_input("Please enter how many random lottery numbers from 1 - 39 would you like to have: "))
+    lotto = (sorted(random.sample(xrange(1, 39), a)))
+    print "Your LOTTERY numbers are %s, Good luck!" % str(lotto)[1:-1]  # shortens string for 1 character on both sides
+    # random.sample(population, k) k = length list of unique elements chosen from the population sequence. xrange for a sample from a range of integers
+    # sorted - sorted list
+
+def main():
+        print "Welcome to the Lottery numbers generator."
+        while True:
+            game = raw_input("1 to start, 2 to quit: ")
+            if game == "1":
+                lottery()
+            elif game == "2":
+                print "Goodbye"
+                break
+
+if __name__ == "__main__":
+    main()
