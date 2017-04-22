1a8ad796b77e9635f382b45a24e7da82b71503c7
ninocka <ninahajtnik@gmail.com>
2017-04-22 12:53:51 +0200

    for loop

--- loto.py
+++ loto.py
@@ -2,7 +2,10 @@ import random
 
 def lottery():
     a = int(raw_input("Please enter how many random lottery numbers from 1 - 39 would you like to have: "))
-    lotto = (sorted(random.sample(xrange(1, 40), a)))
+    lotto = (random.sample(xrange(1, 40), a))
+    for i in lotto:
+        print i
+        lotto = (sorted(random.sample(xrange(1, 40), a)))
     print "Your LOTTERY numbers are %s, Good luck!" % str(lotto)[1:-1]  #str(lotto)[1:-1] shortens string for 1 character on both sides
     # random.sample(population, k) k = length list of unique elements chosen from the population sequence.
     # xrange for a sample from a range of integers
