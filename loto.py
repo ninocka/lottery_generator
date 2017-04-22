9edf4b26f7f801299880ed9cb8558fa567108ece
ninocka <ninahajtnik@gmail.com>
2017-04-22 07:27:18 +0200

    break to exit

--- loto.py
+++ loto.py
@@ -16,7 +16,7 @@ def main():
                 lottery()
             elif game == "2":
                 print "Goodbye"
-                break
+                exit()
 
 if __name__ == "__main__":
     main()
